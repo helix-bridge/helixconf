66fd128f68ed689dd4e8fc65a6958f004a570f7d
JayJay1024 <lygm1062@gmail.com>
2024-07-29 19:28:49 +0800

    feat: token configure logo

--- template/ts/files/tests/helixconf.test.ts
+++ template/ts/files/tests/helixconf.test.ts
@@ -1,11 +1,17 @@
-import {HelixChain} from '../src/'
+import { HelixChain } from "../src/";
 
-describe('helixconf_test', () => {
-  test('test_chainId', () => {
-    console.log(HelixChain.arbitrum.keys());
+describe("helixconf_test", () => {
+  test("test_chainId", () => {
     expect(42161n).toStrictEqual(HelixChain.arbitrum.id);
     expect(46n).toStrictEqual(HelixChain.darwiniaDvm.id);
     expect(44n).toStrictEqual(HelixChain.crabDvm.id);
   });
 });
 
+describe.each(HelixChain.chains())(`$_data.name`, ({ tokens }) => {
+  describe.each(tokens)(`$name`, (token) => {
+    test("The logo field should be configured", () => {
+      expect(token.logo).toBeDefined();
+    });
+  });
+});
