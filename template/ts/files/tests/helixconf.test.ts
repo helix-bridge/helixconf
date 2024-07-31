437c63573d95d0f699f52620e311bc9d45335151
JayJay1024 <lygm1062@gmail.com>
2024-07-31 12:09:53 +0800

    Verify and fix config

--- template/ts/files/tests/helixconf.test.ts
+++ template/ts/files/tests/helixconf.test.ts
@@ -163,14 +163,14 @@ describe("helixconf_test", () => {
   });
 });
 
-describe.each(HelixChain.chains())(`$_data.name`, ({ tokens, code, rpcs, couples }) => {
+describe.each(HelixChain.chains())(`$_data.name`, ({ tokens, code, id, rpcs, couples }) => {
   if (couples.length) {
     test('Native token should be configured', () => {
       expect(tokens.some((t) => t.type === 'native')).toBeTruthy();
     });
   }
 
-  describe.each(tokens)(`$name`, (token) => {
+  describe.each(tokens)(`$symbol`, (token) => {
     test("The logo field should be configured", () => {
       expect(token.logo).toBeDefined();
     });
@@ -178,7 +178,7 @@ describe.each(HelixChain.chains())(`$_data.name`, ({ tokens, code, rpcs, couples
     if (code === "taiko" || code === "bera") {
       // Oops
     } else {
-      test.skip("Should configure the correct decimals", async () => {
+      test("Should configure the correct decimals and symbol", async () => {
         if (token.type === "native") {
           expect(token.decimals).toBe(18);
         } else {
@@ -191,21 +191,35 @@ describe.each(HelixChain.chains())(`$_data.name`, ({ tokens, code, rpcs, couples
           );
           const contractDecimals = await contract.decimals();
           expect(contractDecimals).toBe(BigInt(token.decimals));
+          
+          if (id.toString() === '1') {
+            // https://etherscan.io/address/0x9469D013805bFfB7D3DEBe5E7839237e535ec483#readContract#F8
+          } else {
+            expect(await contract.symbol()).toBe(token.symbol);
+          }
         }
       });
     }
   });
 
-  describe.each(couples)(`Couple $chain.code::$protocol.name::$symbol.to`, (couple) => {
+  describe.each(couples)(`Couple $chain.code::$protocol.name::$symbol.from::$symbol.to`, (couple) => {
     test("Target chain should be configured", () => {
       expect(HelixChain.chains().findIndex((c) => c.code === couple.chain.code)).not.toBe(-1);
+    });
+
+    test("The target chain should not be configured as the source chain", () => {
+      expect(couple.chain.code).not.toBe(code)
+    })
+
+    test("Source token should be configured", () => {
+      expect(tokens.findIndex((t) => t.symbol === couple.symbol.from)).not.toBe(-1);
     });
 
     const targetChain = HelixChain.chains().find((c) => c.code === couple.chain.code);
     if (targetChain) {
       test("Target token should be configured", () => {
         expect(targetChain.tokens.findIndex((t) => t.symbol === couple.symbol.to)).not.toBe(-1);
-      })
+      });
     }
   })
 });
